ad752dde9126ca6ab28e567e3d780fee2dc37ef9
huttongrabiel <huttonthomas@icloud.com>
2021-09-05 10:33:50 -0700

    adding update_balance(), user_transaction(),and user_id...() functions

--- bank_operations.cpp
+++ bank_operations.cpp
@@ -8,31 +8,33 @@
 
 using namespace std;
 
+long double user_transaction();
+
 int main() {
-  long double deposit_amount, withdrawl_amount, balance;
-  balance = 10000;
+  
+}
+
+long double user_transaction() {
   
   string transaction_decision = "";
   cout << "Deposit or Withdrawl?: ";
   cin >> transaction_decision;
-  
+   
+  long double value_change = 0; 
+
   transform(transaction_decision.begin(), transaction_decision.end(), transaction_decision.begin(), ::tolower);
 
   if (transaction_decision == "deposit") {
     cout << "Enter amount to be deposited: ";
-    cin >> deposit_amount;
-    balance += deposit_amount;
+    cin >> value_change;
   }
   else {
     cout << "Enter amount to be withdrawn: ";
-    cin >> withdrawl_amount;
-    if (withdrawl_amount > balance) {
+    cin >> value_change;
+    if (value_change > balance) {
       cout << "Transaction is too large, not enough money in account" << "\n";
     }
-    else {
-      balance -= withdrawl_amount;
-    }
+    value_change *= -1;
   }
-  cout << fixed << "Your updated balance is: " << setprecision(2) << balance << "\n";
-  cout << "Have a good day" << "\n";
+  return value_change;
 }
--- yml_data_access_and_updating.cpp
+++ yml_data_access_and_updating.cpp
@@ -9,7 +9,8 @@ balance is updated as well as a transaction count. */
 
 using namespace std;
 
-void update_balance(long double balance_change_amount, bool withdraw_or_deposit);
+void update_balance(long double value_change);
+void user_id_to_bank_account_connection(long double balance_change_amount, bool withdraw_or_deposit, long int user_id);
 
 int main() {
   update_balance(45, true);
@@ -18,12 +19,11 @@ int main() {
 // Withdraw == true and deposit == false
 // For withdraw_or_deposit: true == withdraw, false == deposit
   
-void user_id_to_bank_account_connection(long double balance_change_amount, bool withdraw_or_deposit) {
+void user_id_to_bank_account_connection(long double balance_change_amount, bool withdraw_or_deposit, long int user_id) {
 
   /* This function uses the user provided log in information to update their balance within the yaml data file. */
   
   long double current_balance = 0;
-  long int user_login_id = 0;
 
   YAML::Node source = YAML::LoadFile("yml/user_accounts.yml");
 
@@ -33,13 +33,20 @@ void user_id_to_bank_account_connection(long double balance_change_amount, bool 
       YAML::Node value = key_value.second;
       if (key.as<string>() == "account_id") {
         // We want the balance value associated with the p value here
-        if (value == user_login_id) {
-          update_balance(); 
+        if (value == user_id) {
+          update_balance(user_transaction()); // Update stored user balance based on the amount they change in user_transaction 
         } 
+      }
+      else {
+        continue;
       }
     }
   }
 
   // Add function to save user last login, transaction count, etc. in addition to the update_balance
   // associated with a user 
+}
+
+void update_balance(long double value_change, long int user_id) {
+   
 }
