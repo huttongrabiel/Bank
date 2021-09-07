1a1cfe3b3a4fbc83c56a05056bd08f4394bc25fa
huttongrabiel <huttonthomas@icloud.com>
2021-09-07 16:56:47 -0700

    removed deleted function from header file

--- cpp/yamlIncludes.h
+++ cpp/yamlIncludes.h
@@ -2,7 +2,6 @@
 #define BANK_INCLUDE_H
 
 void user_balance_update(long double balance_change_amount, std::string username);
-YAML::Node connect_to_yml_data();
 long double user_transaction();
 
 #endif
