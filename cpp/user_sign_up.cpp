8f784ebb6b3986871e4132d20f4cba2ca5b06b6b
huttongrabiel <huttonthomas@icloud.com>
2021-09-08 17:20:37 -0700

    created user_sign_up.cpp to make signups happen

---
+++ cpp/user_sign_up.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <ymlIncludes.h>
+#include <yaml-cpp/yaml.h>
+#include <fstream>
+
+void create_account() {
+  // If a user does not have an account when user_in_database() is called in main.cpp 
+  // it will trigger this function which will sign a user up with all necessary information
+  // and add the user to the yml database
+}
