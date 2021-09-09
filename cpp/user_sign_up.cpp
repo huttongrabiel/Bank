6ac3a7df7537bbd6c1baec03414f9039ae460a0f
huttongrabiel <huttonthomas@icloud.com>
2021-09-08 17:30:13 -0700

    updated header file with create_account() function, started work on create_account() function

--- cpp/user_sign_up.cpp
+++ cpp/user_sign_up.cpp
@@ -3,8 +3,11 @@
 #include <yaml-cpp/yaml.h>
 #include <fstream>
 
-void create_account() {
+void create_account(string new_username) {
   // If a user does not have an account when user_in_database() is called in main.cpp 
   // it will trigger this function which will sign a user up with all necessary information
   // and add the user to the yml database
+
+  YAML::Node source = YAML::LoadFile("../yml/user_accounts.yml");
+  source["users"].push_back(new_username);
 }
