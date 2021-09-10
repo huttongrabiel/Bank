89ed7baf9ef5d2e4423e7eaf91d314b2b086590f
huttongrabiel <huttonthomas@icloud.com>
2021-09-09 21:03:47 -0700

    working on user_sign_up() function, need to figure out how to add sequence of mappings

--- cpp/user_sign_up.cpp
+++ cpp/user_sign_up.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
-#include <ymlIncludes.h>
+#include "yamlIncludes.h"
 #include <yaml-cpp/yaml.h>
 #include <fstream>
 
-void create_account(string new_username) {
+int main() {
   // If a user does not have an account when user_in_database() is called in main.cpp 
   // it will trigger this function which will sign a user up with all necessary information
   // and add the user to the yml database
-
+  std::string new_username = "raphael";
   YAML::Node source = YAML::LoadFile("../yml/user_accounts.yml");
-  source["users"].push_back(new_username);
+  source["users"][new_username].push_back(source["users"][new_username]["account_id"]);
+
+  std::ofstream user_accounts_file;
+  user_accounts_file.open("../yml/user_accounts.yml");
+  user_accounts_file << source;
+  user_accounts_file.close();
 }
