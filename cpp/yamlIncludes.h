19a2efba42d4f8e43541149c8913905acd293b65
huttongrabiel <huttonthomas@icloud.com>
2021-09-11 10:04:34 -0700

    added transaction_receipt() function to header file

--- cpp/yamlIncludes.h
+++ cpp/yamlIncludes.h
@@ -5,5 +5,6 @@ void user_balance_update(long double balance_change_amount, std::string username
 long double user_transaction();
 bool user_in_database(std::string);
 bool username_matches_password(std::string, std::string);
+void transaction_receipt(std::string, long double);
 
 #endif
