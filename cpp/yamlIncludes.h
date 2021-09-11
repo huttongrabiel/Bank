8190d9fa8f36ae9938d48b897d2eb6109c03bb91
huttongrabiel <huttonthomas@icloud.com>
2021-09-11 09:18:28 -0700

    added new function 'user_matches_password()' to header file

--- cpp/yamlIncludes.h
+++ cpp/yamlIncludes.h
@@ -4,5 +4,6 @@
 void user_balance_update(long double balance_change_amount, std::string username);
 long double user_transaction();
 bool user_in_database(std::string);
+bool username_matches_password(std::string, std::string);
 
 #endif
